82fbf78106d8146205f5571cc53530d8b59abe81
YuraKovalevich <kovalevichyura@gmail.com>
2023-03-25 21:23:15 +0300

    feat:creat food, fix map, pacman can eat

--- script/game.js
+++ script/game.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 
-const pacmanFrames = document.getElementsByClassName("animations");
-const ghostFrames = document.getElementsByClassName("ghost");
+const pacmanFrames = document.getElementById("animations");
+const ghostFrames = document.getElementById("ghost");
 
 let createRect = (x, y, width, height, color) => {
     canvasContext.fillStyle = color;
@@ -10,10 +10,18 @@ let createRect = (x, y, width, height, color) => {
 };
 let fps = 30;
 let oneBlockSize = 20;
+let pacman;
 let wallColor = "#3420CA"
 let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
+let foodColor = "#FEB897"
+let score = 0;
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
 
 let map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -47,63 +55,113 @@ let gameLoop = () => {
 };
 
 let update = () => {
+    pacman.moveProcess();
+    pacman.eat();
+
 };
 
+let drawFoods = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 2) {
+                createRect(
+                    j * oneBlockSize + oneBlockSize / 3,
+                    i * oneBlockSize + oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    oneBlockSize / 3,
+                    "#FEB897"
+                );
+            }
+        }
+    }
+}
 let draw = () => {
     createRect(0, 0, canvas.width, canvas.height, "black")
     drawWalls();
+    drawFoods();
+    pacman.draw();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
 
 let drawWalls = () => {
     for (let i = 0; i < map.length; i++) {
-        for (let j = 0; j < map.length; j++) {
-            if (map[i][j] === 1) {
-                createRect(j * oneBlockSize,
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] == 1) {
+                createRect(
+                    j * oneBlockSize,
                     i * oneBlockSize,
                     oneBlockSize,
                     oneBlockSize,
                     wallColor
                 );
-                if (j > 0 && map[i][j - 1] === 1) {
-                    createRect(
-                        j * oneBlockSize,
-                        i * oneBlockSize + wallOffset,
-                        wallSpaceWidth + wallOffset,
-                        wallSpaceWidth,
-                        wallInnerColor
-                    );
-                }
-                if (j < map[0].length - 1 && map[i][j + 1] === 1) {
-                    createRect(
-                        j * oneBlockSize + wallOffset,
-                        i * oneBlockSize + wallOffset,
-                        wallSpaceWidth + wallOffset,
-                        wallSpaceWidth,
-                        wallInnerColor
-                    );
-                }
-                if (i > 0 && map[i - 1][j] === 1) {
-                    createRect(
-                        j * oneBlockSize + wallOffset,
-                        i * oneBlockSize,
-                        wallSpaceWidth,
-                        wallSpaceWidth + wallOffset,
-                        wallInnerColor
-                    );
-                }
-                if (i < map[0].length - 1 && map[i + 1][j] === 1) {
-                    createRect(
-                        j * oneBlockSize + wallOffset,
-                        i * oneBlockSize + wallOffset,
-                        wallSpaceWidth,
-                        wallSpaceWidth + wallOffset,
-                        wallInnerColor
-                    );
-                }
+            }
+            if (j > 0 && map[i][j - 1] == 1) {
+                createRect(
+                    j * oneBlockSize,
+                    i * oneBlockSize + wallOffset,
+                    wallSpaceWidth + wallOffset,
+                    wallSpaceWidth,
+                    wallInnerColor
+                );
+            }
+            if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+                createRect(
+                    j * oneBlockSize + wallOffset,
+                    i * oneBlockSize + wallOffset,
+                    wallSpaceWidth + wallOffset,
+                    wallSpaceWidth,
+                    wallInnerColor
+                );
+            }
+            if (i > 0 && map[i - 1][j] == 1) {
+                createRect(
+                    j * oneBlockSize + wallOffset,
+                    i * oneBlockSize,
+                    wallSpaceWidth,
+                    wallSpaceWidth + wallOffset,
+                    wallInnerColor
+                );
+            }
+            if (i < map[0].length - 1 && map[i + 1][j] == 1) {
+                createRect(
+                    j * oneBlockSize + wallOffset,
+                    i * oneBlockSize + wallOffset,
+                    wallSpaceWidth,
+                    wallSpaceWidth + wallOffset,
+                    wallInnerColor
+                );
             }
         }
     }
 
 }
+
+let createNewPacman = () => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5
+    );
+};
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+    let k = event.keyCode;
+
+    setTimeout(() => {
+        if (k == 37 || k == 65) {
+            pacman.nextDirection = DIRECTION_LEFT;
+        } else if (k == 38 || k == 87) {
+            pacman.nextDirection = DIRECTION_UP;
+        } else if (k == 39 || k == 68) {
+            pacman.nextDirection = DIRECTION_RIGHT;
+        } else if (k == 40 || k == 83) {
+            pacman.nextDirection = DIRECTION_BOTTOM;
+        }
+    }, 1)
+})
